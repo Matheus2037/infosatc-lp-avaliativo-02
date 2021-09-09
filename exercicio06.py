cef45a85410ca75b1e68afb2a7e0a271c924a8d2
Matheus2037 <matheusdasilvagastaldi@gmail.com>
2021-09-09 16:01:03 -0300

    exercicio06
    mais um feito:)

---
+++ exercicio06.py
@@ -0,0 +1,5 @@
+lista = ['chocolate', 'salgadinho', 'leite condensado', 'água', 'batata', 'leite', 'camarão']
+
+n1 = input("Digite o item que deseja saber a posição: ")
+
+print("O item escolhido está na posição: ", lista.index(n1))

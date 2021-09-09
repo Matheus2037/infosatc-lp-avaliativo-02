7b70ddccf1c53a7863f7575604acd27a7ad1ad9b
Matheus2037 <matheusdasilvagastaldi@gmail.com>
2021-09-09 17:18:23 -0300

    exercicio07
    update do exercicio07

--- exercicio07.py
+++ exercicio07.py
@@ -2,47 +2,47 @@ lista_filmes = ['vingadores ultimato', 'star wars', 'as branquelas', 'vovó... z
 print("lista filmes: " ,lista_filmes)
 print("\n")
 lista_jogos = ['genshin impact', 'the witcher 3', 'dying light', 'destiny']#Criando as 4 listas
-print(lista_jogos)
+print("lista jogos: " ,lista_jogos)
 print("\n")
 lista_livros = ['o ultimo desejo', 'a espada do destino', 'o sangue dos elfos', 'a torre da andorinha']#Criando as 4 listas
-print(lista_livros)
+print("lista livros: " ,lista_livros)
 print("\n")
 lista_esportes = ['futebol', 'vôlei', 'basquete']#Criando as 4 listas
-print(lista_esportes)
+print("lista esportes: " ,lista_esportes)
 
 print("\n")
 
 lista_filmes.append("carros 2") #adicionando 2 itens nas listas
 lista_filmes.append("o exterminador do futuro")#adicionando 2 itens nas listas
-print(lista_filmes)
+print("lista filme atualizada: " ,lista_filmes)
 print("\n")
 lista_jogos.append("minecraft")#adicionando 2 itens nas listas
 lista_jogos.append("terraria")#adicionando 2 itens nas listas
-print(lista_jogos)
+print("lista jogos atualizada: " ,lista_jogos)
 print("\n")
 lista_livros.append("tempo do desprezo")#adicionando 2 itens nas listas
 lista_livros.append("a dama do lago")#adicionando 2 itens nas listas
-print(lista_livros)
+print("lista livros atualizada: " ,lista_livros)
 print("\n")
 lista_esportes.append("ginastica")#adicionando 2 itens nas listas
 lista_esportes.append("salto com vara")#adicionando 2 itens nas listas
-print(lista_esportes)
+print("lista esportes atualizada: " ,lista_esportes)
 print("\n")
 
 
 
-lista_geral = ['lista_filmes', 'lista_jogos', 'lista_livros', 'lista_esportes']#lista com todas as outras listas
-print(lista_geral)
+lista_geral = [lista_filmes, lista_jogos, lista_livros, lista_esportes]#lista com todas as outras listas
+print("lista geral atualizada: " ,lista_geral)
 print("\n")
 
 
-print(lista_livros[1])#mostrando algum item da lista livros
+print("lista livros atualizada: " ,lista_livros[1])#mostrando algum item da lista livros
 print("\n")
 
 
 del lista_esportes[0] #removendo item da lista esporte
-print(lista_esportes)
+print("livros esportes atualizada: " ,lista_esportes)
 print("\n")
 
-lista_geral.append("lista_disciplina")#criando uma nova lista disciplina na lista onde tem todas as outras
-print(lista_geral)
+lista_geral.append("lista_disciplina =['matematica', 'portugues', 'ingles', programação]")#criando uma nova lista disciplina na lista onde tem todas as outras
+print("lista geral atualizada: ",lista_geral)

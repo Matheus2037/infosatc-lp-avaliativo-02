e02c062b55f4d6dc5566f31c19e36ef646cf96b2
Matheus2037 <matheusdasilvagastaldi@gmail.com>
2021-09-09 15:48:11 -0300

    exercicio03
    feito:)

---
+++ exercicio03.py
@@ -0,0 +1,8 @@
+n1 = float(input("Digite um valor para por na lista: "))
+n2 = float(input("Digite um valor para por na lista: "))
+n3 = float(input("Digite um valor para por na lista: "))
+n4 = float(input("Digite um valor para por na lista: "))
+
+lista = [n1, n2, n3, n4]
+
+print("A soma dos valores dentro da lista é de: " ,sum(lista))

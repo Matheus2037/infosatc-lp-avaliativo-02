d59c027313b5d7b0732969896869dab2b3418b38
Matheus2037 <matheusdasilvagastaldi@gmail.com>
2021-09-09 16:51:35 -0300

    exercicio08
    :)

---
+++ exercicio08.py
@@ -0,0 +1,18 @@
+peça = input("Digite qual peça voce escolheu: ")
+peça2 = input("Digite qual peça voce escolheu: ")
+peça3 = input("Digite qual peça voce escolheu: ")
+peça4 = input("Digite qual peça voce escolheu: ")
+peça5 = input("Digite qual peça voce escolheu: ")
+peça6 = input("Digite qual peça voce escolheu: ")
+peça7 = input("Digite qual peça voce escolheu: ")
+peça8 = input("Digite qual peça voce escolheu: ")
+
+lista = [peça, peça2, peça3, peça4, peça5, peça6, peça7, peça8, 'placa de video RTX 2080ti', 'gabinete cooler master']
+print(lista)
+print("\n")
+
+
+del lista[8]#removendo os itens que ultrapassam o valor desejado
+del lista[8] #removendo os itens que ultrapassam o valor desejado
+print(lista)
+print("\n")

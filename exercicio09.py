696a720d7fddc847e25f4dc7244a5e5212a78b1c
Matheus2037 <matheusdasilvagastaldi@gmail.com>
2021-09-09 16:55:23 -0300

    exercicio09
    :)

---
+++ exercicio09.py
@@ -0,0 +1,8 @@
+n1 = int(input("Digite um número inteiro: "))
+n2 = int(input("Digite um número inteiro: "))
+n3 = int(input("Digite um número inteiro: "))
+n4 = int(input("Digite um número inteiro: "))
+
+lista = [n1, n2, n3, n4]
+
+print("minha lista ordenada numérica: ", sorted(lista))
